618039a1e57b94bb27c1e4430d2c6877471fbc0e
gkstmdgns422 <hanseunghun422@gmail.com>
2021-08-03 10:02:51 +0900

    주석 추가하였습니다

--- b11399.py
+++ b11399.py
@@ -1,9 +1,12 @@
 people_num = int(input())
 time = input()
 
-time = list(map(int,time.split()))
+# 리스트로 형변환
+time = list(map(int,time.split())) 
+# 오름차순 정렬
 time.sort()
+# 인출하는데 걸리는 시간의 합구하기
 total = 0
 for i in range(people_num+1):
-    total += sum(time[:i])
+    total += sum(time[:i]) 
 print(total)
